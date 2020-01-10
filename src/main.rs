1540c4877aa6738d1e7123d5d346cdd774ce7cd4
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2020-01-10 12:58:10 -0800

    plot 1 TB

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ pub const PIECE_SIZE: usize = 4096;
 pub const ID_SIZE: usize = 32;
 pub const BLOCK_SIZE: usize = 16;
 pub const BLOCKS_PER_PIECE: usize = PIECE_SIZE / BLOCK_SIZE;
-pub const PLOT_SIZES: [usize; 4] = [
-    256,        // 1 MB
-    256 * 100,      // 100 MB
-    256 * 1000,     // 1 GB
-    256 * 1000 * 100,   // 100 GB
-    // 256 * 1000 * 1000,  // 1 TB
+pub const PLOT_SIZES: [usize; 1] = [
+    // 256,        // 1 MB
+    // 256 * 100,      // 100 MB
+    // 256 * 1000,     // 1 GB
+    // 256 * 1000 * 100,   // 100 GB
+    256 * 1000 * 1000,  // 1 TB
 ];
 pub const ROUNDS: usize = 2048;
 pub const PIECES_PER_BATCH: usize = 8;
