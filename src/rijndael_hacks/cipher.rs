8b920768065d952b77c7f20d40ce96fdea0bdd1f
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-01-19 20:35:50 +0200

    Generalize code a bit, allowing using arbitrary slices of 128 bits slices

--- src/rijndael_hacks/cipher.rs
+++ src/rijndael_hacks/cipher.rs
@@ -2,7 +2,6 @@ use super::expand;
 use crate::arch::*;
 use block_cipher_trait::generic_array::typenum::{U16, U32, U8};
 use block_cipher_trait::generic_array::GenericArray;
-use block_cipher_trait::BlockCipher;
 use core::mem;
 
 // Code below is a refactored version of aesni crate that doesn't use any macros
@@ -26,55 +25,59 @@ impl Cipher {
         }
     }
 
-    fn encrypt_blocks_dynamic(&self, blocks: &mut Block128x8, parallel_blocks: usize) {
+    /// Caller of this method must ensure that number of elements in `blocks` argument corresponds
+    /// to `parallel_blocks` argument provided
+    pub unsafe fn encrypt_blocks_dynamic(&self, blocks: &mut [Block128], parallel_blocks: usize) {
         let keys = self.encrypt_keys;
-        unsafe {
-            let mut b: [__m128i; 32] = mem::uninitialized();
+        let mut b: [__m128i; 32] = mem::uninitialized();
+        for block in 0..parallel_blocks {
+            b[block] = _mm_loadu_si128(blocks[block].as_ptr() as *const __m128i);
+            b[block] = _mm_xor_si128(b[block], keys[0]);
+        }
+        for round in 1..=13 {
             for block in 0..parallel_blocks {
-                b[block] = _mm_loadu_si128(blocks[block].as_ptr() as *const __m128i);
-                b[block] = _mm_xor_si128(b[block], keys[0]);
+                b[block] = _mm_aesenc_si128(b[block], keys[round]);
             }
-            for round in 1..=13 {
-                for block in 0..parallel_blocks {
-                    b[block] = _mm_aesenc_si128(b[block], keys[round]);
-                }
-            }
-            for block in 0..parallel_blocks {
-                b[block] = _mm_aesenclast_si128(b[block], keys[14]);
-            }
-            for block in 0..parallel_blocks {
-                _mm_storeu_si128(blocks[block].as_mut_ptr() as *mut __m128i, b[block]);
-            }
+        }
+        for block in 0..parallel_blocks {
+            b[block] = _mm_aesenclast_si128(b[block], keys[14]);
+        }
+        for block in 0..parallel_blocks {
+            _mm_storeu_si128(blocks[block].as_mut_ptr() as *mut __m128i, b[block]);
         }
     }
 
-    fn decrypt_blocks_dynamic(&self, blocks: &mut Block128x8, parallel_blocks: usize) {
+    /// Caller of this method must ensure that number of elements in `blocks` argument corresponds
+    /// to `parallel_blocks` argument provided
+    pub unsafe fn decrypt_blocks_dynamic(&self, blocks: &mut [Block128], parallel_blocks: usize) {
         let keys = self.decrypt_keys;
-        unsafe {
-            let mut b: [__m128i; 32] = mem::uninitialized();
+        let mut b: [__m128i; 32] = mem::uninitialized();
+        for block in 0..parallel_blocks {
+            b[block] = _mm_loadu_si128(blocks[block].as_ptr() as *const __m128i);
+            b[block] = _mm_xor_si128(b[block], keys[14]);
+        }
+        for round in (1..=13).rev() {
             for block in 0..parallel_blocks {
-                b[block] = _mm_loadu_si128(blocks[block].as_ptr() as *const __m128i);
-                b[block] = _mm_xor_si128(b[block], keys[14]);
+                b[block] = _mm_aesdec_si128(b[block], keys[round]);
             }
-            for round in (1..=13).rev() {
-                for block in 0..parallel_blocks {
-                    b[block] = _mm_aesdec_si128(b[block], keys[round]);
-                }
-            }
-            for block in 0..parallel_blocks {
-                b[block] = _mm_aesdeclast_si128(b[block], keys[0]);
-            }
-            for block in 0..parallel_blocks {
-                _mm_storeu_si128(blocks[block].as_mut_ptr() as *mut __m128i, b[block]);
-            }
+        }
+        for block in 0..parallel_blocks {
+            b[block] = _mm_aesdeclast_si128(b[block], keys[0]);
+        }
+        for block in 0..parallel_blocks {
+            _mm_storeu_si128(blocks[block].as_mut_ptr() as *mut __m128i, b[block]);
         }
     }
 
     pub fn encrypt_blocks(&self, blocks: &mut Block128x8) {
-        self.encrypt_blocks_dynamic(blocks, 8);
+        unsafe {
+            self.encrypt_blocks_dynamic(blocks.as_mut_slice(), 8);
+        }
     }
 
     pub fn decrypt_blocks(&self, blocks: &mut Block128x8) {
-        self.decrypt_blocks_dynamic(blocks, 8);
+        unsafe {
+            self.decrypt_blocks_dynamic(blocks.as_mut_slice(), 8);
+        }
     }
 }
