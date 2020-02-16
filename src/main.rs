858cd5aa0d347ad7a0b0881d9927f0b6d3f24aa1
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-02-16 21:33:21 +0200

    Separate struct with AES encryption/decryption using OpenCL

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
-use ocl::core::{
-    build_program, create_buffer, create_command_queue, create_context, create_kernel,
-    create_program_with_source, enqueue_kernel, enqueue_read_buffer, enqueue_write_buffer,
-    set_kernel_arg, ArgVal, ContextProperties, Event, Uchar16, Uint,
-};
-use ocl::flags;
-use ocl::Device;
-use ocl::Platform;
-use std::convert::TryInto;
-use std::ffi::CString;
-use std::ops::Deref;
 use std::time::Instant;
 
+mod aes_open_cl;
 mod aes_soft;
 
-const AES_OPENCL: &str = include_str!("aes_kernels.cl");
 const ROUND_KEYS: usize = 60;
 const BLOCK_SIZE: usize = 16;
-const ITERATIONS: usize = 10_240_000;
-
-fn u8_slice_to_uchar16_vec(input: &[u8]) -> Vec<Uchar16> {
-    assert_eq!(input.len() % 4, 0);
-
-    input
-        .chunks_exact(16)
-        .map(|chunk| chunk.try_into().unwrap())
-        .map(|chunk: [u8; 16]| Uchar16::from(chunk))
-        .collect()
-}
-
-fn u32_slice_to_uint_vec(input: &[u32]) -> Vec<Uint> {
-    input
-        .iter()
-        .map(|chunk| Uint::from(chunk.to_owned()))
-        .collect()
-}
+const ITERATIONS: usize = 10;
 
 fn main() -> ocl::Result<()> {
     let key = vec![
@@ -45,130 +17,30 @@ fn main() -> ocl::Result<()> {
     ];
 
     let mut keys = [0u32; ROUND_KEYS];
-    aes_soft::setkey_enc_k256(&key, &mut keys);
+    aes_soft::setkey_dec_k256(&key, &mut keys);
 
     let mut res = [0u8; BLOCK_SIZE];
     {
         let start = Instant::now();
         for _ in 0..ITERATIONS {
-            aes_soft::block_enc_k256(&block, &mut res, &keys);
+            aes_soft::block_dec_k256(&block, &mut res, &keys);
         }
         println!("{}us", start.elapsed().as_micros());
     }
     println!("Correct result: {:?}", res);
 
-    // Init start
-    let platform = Platform::first()?;
+    let aes_256_opencl = aes_open_cl::Aes256OpenCL::new()?;
 
-    let device = Device::first(platform)?;
-
-    let context_properties = ContextProperties::new().platform(platform);
-    let context = create_context(Some(&context_properties), &[&device], None, None)?;
-
-    let queue = create_command_queue(&context, &device, None)?;
-
-    let program = create_program_with_source(&context, &[CString::new(AES_OPENCL)?])?;
-
-    let options = CString::new("").unwrap();
-    build_program(&program, Some(&[&device]), &options, None, None)?;
-
-    let encrypt_kernel = create_kernel(&program, "aes_256")?;
-
-    let buffer_in = unsafe {
-        create_buffer(
-            &context,
-            flags::MEM_READ_ONLY,
-            BLOCK_SIZE * ITERATIONS,
-            None::<&[Uchar16]>,
-        )?
-    };
-    let buffer_out = unsafe {
-        create_buffer(
-            &context,
-            flags::MEM_WRITE_ONLY,
-            BLOCK_SIZE * ITERATIONS,
-            None::<&[Uchar16]>,
-        )?
-    };
-    let buffer_round_keys =
-        unsafe { create_buffer(&context, flags::MEM_READ_ONLY, ROUND_KEYS, None::<&[Uint]>)? };
-
-    set_kernel_arg(&encrypt_kernel, 0, ArgVal::mem(&buffer_in))?;
-    set_kernel_arg(&encrypt_kernel, 1, ArgVal::mem(&buffer_out))?;
-    set_kernel_arg(&encrypt_kernel, 2, ArgVal::mem(&buffer_round_keys))?;
-    // Init end
-
-    {
-        let mut event = Event::null();
-        unsafe {
-            enqueue_write_buffer(
-                &queue,
-                &buffer_in,
-                true,
-                0,
-                &u8_slice_to_uchar16_vec(
-                    &(0..ITERATIONS)
-                        .flat_map(|_| &block)
-                        .map(|x| x.to_owned())
-                        .collect::<Vec<u8>>(),
-                ),
-                None::<Event>,
-                Some(&mut event),
-            )?;
-        }
-    }
-
-    {
-        let mut event = Event::null();
-        unsafe {
-            enqueue_write_buffer(
-                &queue,
-                &buffer_round_keys,
-                true,
-                0,
-                &u32_slice_to_uint_vec(&keys),
-                None::<Event>,
-                Some(&mut event),
-            )?;
-        }
-    }
-
+    let mut output = Default::default();
     {
         let start = Instant::now();
         for _ in 0..1 {
-            unsafe {
-                enqueue_kernel(
-                    &queue,
-                    &encrypt_kernel,
-                    1,
-                    None,
-                    &[ITERATIONS, 0, 0],
-                    None,
-                    None::<Event>,
-                    None::<&mut Event>,
-                )
-            }?;
+            output = aes_256_opencl.decrypt(&block, &keys)?;
         }
         println!("{}us", start.elapsed().as_micros());
     }
 
-    let mut res = vec![Uchar16::from([0u8; BLOCK_SIZE])];
-    {
-        let mut event = Event::null();
-        unsafe {
-            enqueue_read_buffer(
-                &queue,
-                &buffer_out,
-                true,
-                0,
-                &mut res,
-                None::<Event>,
-                Some(&mut event),
-            )?;
-        };
-    }
-
-    println!("GPU result: {:?}", res);
+    println!("GPU result: {:?}", output);
 
     Ok(())
 }
