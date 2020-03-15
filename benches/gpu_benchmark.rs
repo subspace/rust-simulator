3b57b41c8d72144602fec7a44e043d33c37f31f1
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-03-15 20:29:46 +0200

    PoR encode benchmark

--- benches/gpu_benchmark.rs
+++ benches/gpu_benchmark.rs
@@ -1,36 +1,34 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::Criterion;
 use rust_simulator::aes_soft;
 use rust_simulator::codec::Codec;
+use rust_simulator::crypto;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut aes_256_open_cl = Codec::new().unwrap();
+    let mut codec = Codec::new().unwrap();
 
-    let key = vec![
-        210, 51, 245, 243, 109, 154, 58, 127, 99, 229, 195, 34, 103, 170, 183, 16, 61, 83, 196,
-        156, 124, 20, 16, 161, 3, 25, 180, 170, 26, 19, 163, 12,
-    ];
-    let mut keys = [0u32; 60];
-    aes_soft::setkey_enc_k256(&key, &mut keys);
-    let block = vec![
-        206, 213, 196, 136, 255, 138, 90, 170, 236, 76, 241, 48, 122, 18, 42, 189,
-    ];
+    let id = crypto::random_bytes_32();
+    let mut keys = [0u32; 44];
+    aes_soft::setkey_enc_k128(&id, &mut keys);
+    let piece = crypto::random_bytes_4096();
+    let iv = vec![13u128];
 
     let mut group = c.benchmark_group("GPU benchmark");
     group.sample_size(10);
 
-    group.bench_function("encode-single", |b| {
+    group.bench_function("PoR-128-encode-single", |b| {
         b.iter(|| {
-            aes_256_open_cl
-                .aes_256_enc_iterations(&block, &keys, 1)
-                .unwrap();
+            codec.por_128_enc(&piece, &iv, &keys).unwrap();
         })
     });
 
-    group.bench_function("encode-66k", |b| {
+    let pieces: Vec<u8> = (0..100).flat_map(|_| piece.to_vec()).collect();
+    let ivs = vec![13u128; 100];
+
+    group.bench_function("PoR-128-encode-100", |b| {
         b.iter(|| {
-            aes_256_open_cl
-                .aes_256_enc_iterations(&block, &keys, 66000)
-                .unwrap();
+            codec.por_128_enc(&pieces, &ivs, &keys).unwrap();
         })
     });
 
